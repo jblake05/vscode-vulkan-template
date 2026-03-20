0a2c457e30dd1215de35ba2d316c4ff065113b19
jblake05 <125399868+jblake05@users.noreply.github.com>
2026-03-19 21:54:52 -0500

    Instance

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ import vulkan_hpp;
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <algorithm>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -36,9 +37,9 @@ private:
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-
     }
-    void createInstance() {
+
+    int createInstance() {
         vk::ApplicationInfo appInfo = {}; 
         appInfo.pApplicationName    = "Hello Triangle",
         appInfo.applicationVersion  = VK_MAKE_VERSION( 1, 0, 0 ),
@@ -46,10 +47,51 @@ private:
         appInfo.engineVersion       = VK_MAKE_VERSION( 1, 0, 0 ),
         appInfo.apiVersion          = vk::ApiVersion14;
 
-        vk::InstanceCreateInfo createInfo = {};
-        createInfo.pApplicationInfo       = &appInfo;
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-        instance = vk::raii::Instance(context, createInfo);
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {            
+            if (std::ranges::none_of(extensionProperties,
+                                    [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                                    { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+
+        vk::InstanceCreateInfo createInfo  = {};
+        createInfo.pApplicationInfo        = &appInfo;
+        createInfo.enabledExtensionCount   = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        try {
+            auto extensions = context.enumerateInstanceExtensionProperties();
+            std::cout << "Available extensions:\n";
+
+            for (const auto& extension : extensions) {
+                std::cout << '\t' << extension.extensionName << '\n';
+            }
+
+            instance = vk::raii::Instance(context, createInfo);
+            vk::raii::PhysicalDevice physicalDevice = instance.enumeratePhysicalDevices().front();
+            vk::raii::Device device(physicalDevice, vk::DeviceCreateInfo{});
+
+            vk::raii::Buffer buffer(device, vk::BufferCreateInfo{});
+        }
+        catch (const vk::SystemError& err) {
+            std::cerr << "Vulkan error: " << err.what() << std::endl;
+            return 1;
+        }
+        catch (const std::exception& err) {
+            std::cerr << "Error: " << err.what() << std::endl;
+            return 1;
+        }
+        return 0;
     }
 
     void initVulkan() {
